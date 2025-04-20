0e22cafd96fab28920084f350703a0afb6a3663d
prashant13579 <pashya777@gmail.com>
2025-04-20 05:25:49 +0000

    conversion of dataType

---
+++ basics/dataType.js
@@ -0,0 +1,13 @@
+let score = "33abc"
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof (valueInNumber));
+console.log(valueInNumber);
+
+let isLoggedIn = 1
+let booleanisLoggedInValue = Boolean(isLoggedIn) 
+console.log(booleanisLoggedInValue);
+
+
+
